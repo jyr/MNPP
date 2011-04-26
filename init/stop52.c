d5ab1f8334f795665b3ce87becd4572182771e93
Jair Gaxiola <jyr.gaxiola@gmail.com>
2011-04-26 01:18:24 -0500

    switch for php52 and php53 from UI

---
+++ init/stop52.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+  system("/Applications/MNPP/init/stop 52");
+
+  return 0;
+}
---
+++ init/stop53.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+  system("/Applications/MNPP/init/stop 53");
+
+  return 0;
+}
