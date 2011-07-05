7ed36f1db3fa8b06f5cab83a46dd41f6e21c3a3e
Jair Gaxiola <jyr.gaxiola@gmail.com>
2011-07-04 22:01:28 -0500

    flask example

---
+++ htdocs/flask/index.py
@@ -0,0 +1,14 @@
+#!/Applications/MNPP/Library/python26/bin/python
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return "Hello World from flask!"
+
+def application(environ, start_response):
+  return app(environ, start_response)
+
+
+#if __name__ == '__main__':
+#    app.run()
