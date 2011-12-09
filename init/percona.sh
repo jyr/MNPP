eb0753c077e3768399cd29b789c37359133a7060
Jair Gaxiola <jyr.gaxiola@gmail.com>
2011-12-09 13:05:14 -0600

    return to prompt after start percona

--- init/percona.sh
+++ init/percona.sh
@@ -9,7 +9,7 @@ __show_usage( ) {
 
 case "${1}" in
     start)
-        /Applications/MNPP/Library/mysql/bin/mysqld_safe -u root --port=3306 --socket=/Applications/MNPP/tmp/mysql/mysql.sock --lower_case_table_names=0 --datadir=/Applications/MNPP/Library/mysql/var --pid-file=/Applications/MNPP/tmp/mysql/mysql.pid --log-error=/Applications/MNPP/logs/mysql_error_log --tmpdir=/Applications/MNPP/tmp/mysql &
+        /Applications/MNPP/Library/mysql/bin/mysqld_safe -u root --port=3306 --socket=/Applications/MNPP/tmp/mysql/mysql.sock --lower_case_table_names=0 --datadir=/Applications/MNPP/Library/mysql/var --pid-file=/Applications/MNPP/tmp/mysql/mysql.pid --log-error=/Applications/MNPP/logs/mysql_error_log --tmpdir=/Applications/MNPP/tmp/mysql & sleep 1; exit 0 
         ;;
     stop)
         /Applications/MNPP/Library/mysql/bin/mysqladmin -u root --socket=/Applications/MNPP/tmp/mysql/mysql.sock shutdown
