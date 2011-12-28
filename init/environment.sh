f914ff83bc9e74883cf8eed1e426dd29006b602a
Jair Gaxiola <jyr.gaxiola@gmail.com>
2011-12-28 17:53:41 -0600

    export DYLD_LIBRARY from .bash_profile

--- init/environment.sh
+++ init/environment.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 __export_library( ){
-	touch /Users/$USER/.bash_profile
-	found=`cat /Users/$USER/.bash_profile | grep "$IP $2" | wc -l`
-
+	touch /Users/$SUDO_USER/.bash_profile
+	found=`cat /Users/$SUDO_USER/.bash_profile | grep MNPP | wc -l`
+	export DYLD_LIBRARY_PATH=/Applications/MNPP/init:/Applications/MNPP/Library/lib:$DYLD_LIBRARY_PATH
+	
   	if [ $found = 0 ] ; then
-		echo "alias drush='/Applications/MNPP/Library/php53/bin/php /Applications/MNPP/Library/drush/drush.php'" >> /Users/$USER/.bash_profile
-		echo "export PATH=/Applications/MNPP/init:/Applications/MNPP/Library/php53/bin:/Applications/MNPP/Library/mysql/bin:\$PATH" >> /Users/$USER/.bash_profile
-		echo "export DYLD_LIBRARY_PATH=/Applications/MNPP/init:/Applications/MNPP/Library/lib:\$DYLD_LIBRARY_PATH" >> /Users/$USER/.bash_profile
+		echo "alias drush='/Applications/MNPP/Library/php53/bin/php /Applications/MNPP/Library/drush/drush.php'" >> /Users/$SUDO_USER/.bash_profile
+		echo "export PATH=/Applications/MNPP/init:/Applications/MNPP/Library/php53/bin:/Applications/MNPP/Library/mysql/bin:\$PATH" >> /Users/$SUDO_USER/.bash_profile
+		echo "alias mysql='/Applications/MNPP/Library/mysql/bin/mysql --socket=/Applications/MNPP/tmp/mysql/mysql.sock'" >> /Users/$SUDO_USER/.bash_profile
+		echo "export DYLD_LIBRARY_PATH=/Applications/MNPP/init:/Applications/MNPP/Library/lib:$DYLD_LIBRARY_PATH" >> /Users/$SUDO_USER/.bash_profile
   	fi
 }
 
