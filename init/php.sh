c03804dff355b12ec5a2d57889e0e94d25fd27a9
Jair Gaxiola <jyr.gaxiola@gmail.com>
2011-12-28 17:51:40 -0600

    Add export DYLD_LIBRARY at .bash_profile

--- init/php.sh
+++ init/php.sh
@@ -77,6 +77,7 @@ __export_library( ){
 		echo "alias drush='/Applications/MNPP/Library/php53/bin/php /Applications/MNPP/Library/drush/drush.php'" >> /Users/$SUDO_USER/.bash_profile
 		echo "export PATH=/Applications/MNPP/init:/Applications/MNPP/Library/php53/bin:/Applications/MNPP/Library/mysql/bin:\$PATH" >> /Users/$SUDO_USER/.bash_profile
 		echo "alias mysql='/Applications/MNPP/Library/mysql/bin/mysql --socket=/Applications/MNPP/tmp/mysql/mysql.sock'" >> /Users/$SUDO_USER/.bash_profile
+		echo "export DYLD_LIBRARY_PATH=/Applications/MNPP/init:/Applications/MNPP/Library/lib:$DYLD_LIBRARY_PATH" >> /Users/$SUDO_USER/.bash_profile
   	fi
 }
 
