f67fa9eb30aff6099949212351bfd61ea3ab0db5
Jair Gaxiola <jyr.gaxiola@gmail.com>
2013-04-11 02:10:55 -0500

    compiling libs, percona, php, nginx for OS X 10.7

---
+++ Library/10.7/mysql/mysql-test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Applications/MNPP/src/Percona-Server-5.5.30-rel30.1/mysql-test
+# Build directory: /Applications/MNPP/src/Percona-Server-5.5.30-rel30.1/mysql-test
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
