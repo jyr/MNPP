949c5b0b6fa98844f5993b72939fd364c49d59e5
Jyr Gaxiola <jyr.gaxiola@gmail.com>
2013-04-19 23:53:59 -0500

    mysql initial database

--- Library/mysql/mysql-test/CTestTestfile.cmake
+++ Library/mysql/mysql-test/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /Applications/MNPP/src/jpeg-9/libpng-1.6.1/freetype-2.4.11/gettext-0.18.2/libmcrypt-2.5.8/mcrypt-2.6.8/libxml2-2.8.0/libxslt-1.1.28/libiconv-1.14/mhash-0.9.9.9/gd-2.0.36RC1/Percona-Server-5.5.30-rel30.2/mysql-test
-# Build directory: /Applications/MNPP/src/jpeg-9/libpng-1.6.1/freetype-2.4.11/gettext-0.18.2/libmcrypt-2.5.8/mcrypt-2.6.8/libxml2-2.8.0/libxslt-1.1.28/libiconv-1.14/mhash-0.9.9.9/gd-2.0.36RC1/Percona-Server-5.5.30-rel30.2/mysql-test
+# Source directory: /Applications/MNPP/src/Percona-Server-5.5.30-rel30.2/mysql-test
+# Build directory: /Applications/MNPP/src/Percona-Server-5.5.30-rel30.2/mysql-test
 # 
 # This file includes the relevent testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
