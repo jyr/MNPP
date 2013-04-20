37ffa0869cf49a0485afe0818276d4185d64a758
Jyr Gaxiola <jyr.gaxiola@gmail.com>
2013-04-20 00:28:38 -0500

    creating nginx logs on the fly

--- init/environment.sh
+++ init/environment.sh
@@ -4,14 +4,18 @@ __export_library( ){
 	touch /Users/$SUDO_USER/.bash_profile
 	found=`cat /Users/$SUDO_USER/.bash_profile | grep MNPP | wc -l`
 	export DYLD_LIBRARY_PATH=/Applications/MNPP/init:/Applications/MNPP/Library/lib:$DYLD_LIBRARY_PATH
-  
-  	if [ $found = 0 ] ; then
+ 	NGINX_LOGS='/Applications/MNPP/Library/nginx/logs'
+	
+	if [ ! -d $NGINX_LOGS ]; then
+		mkdir $NGINX_LOGS
+	fi
+
+  if [ $found = 0 ] ; then
 		echo "alias drush='/Applications/MNPP/Library/bin/php /Applications/MNPP/Library/drush/drush.php'" >> /Users/$SUDO_USER/.bash_profile
 		echo "export PATH=/Applications/MNPP/init:/Applications/MNPP/Library/php54/bin:/Applications/MNPP/Library/mysql/bin:\$PATH" >> /Users/$SUDO_USER/.bash_profile
 		echo "alias mysql='/Applications/MNPP/Library/mysql/bin/mysql --socket=/Applications/MNPP/tmp/mysql/mysql.sock'" >> /Users/$SUDO_USER/.bash_profile
 		echo "export DYLD_LIBRARY_PATH=/Applications/MNPP/init:/Applications/MNPP/Library/lib:$DYLD_LIBRARY_PATH" >> /Users/$SUDO_USER/.bash_profile
-		mkdir /Applications/MNPP/Library/nginx/logs
-  	fi
+  fi
 }
 
 __export_library
